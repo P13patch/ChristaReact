a5e2b8f04a40bc4b7f9d80af9f1aa15665b854b1
Patrick Wendel <ptrckwndl13@gmail.com>
2020-01-24 19:07:54 -0700

    Contact styling

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -18,17 +18,17 @@ function Contact() {
                     <div className="nameBox">
                         <div>Name</div>
                         <span>
-                            <input type='text' name='first' style={{height:"2vw"}}></input>
-                            <input type='text' name='last' style={{height:"2vw"}}></input>
+                            <input type='text' name='first' style={{height:"2vw", width:"40%"}}></input>
+                            <input type='text' name='last' style={{height:"2vw", margin:"1vw", width:"40%"}}></input>
                         </span>
                     </div>
                     <div className="emailBox">
                         <div>Email</div>
-                        <input type='text' style={{height:"2vw", width:"20vw"}}></input>
+                        <input type='text' style={{height:"2vw", width:"100%", marginTop:"1vw"}}></input>
                     </div>
                     <div className="commentsBox">
                         <div>Comments</div>
-                        <textarea name='comments' rows='10' cols='60'></textarea>
+                        <textarea name='comments' rows='10' cols='60' style={{marginTop:"1vw"}}></textarea>
                     </div>
                 </form>
             </div>
