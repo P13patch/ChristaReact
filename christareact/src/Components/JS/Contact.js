54cda1b5f81864146cce81602f034c311233e6f9
Patrick Wendel <ptrckwndl13@gmail.com>
2020-01-24 18:36:26 -0700

    Contact page

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -8,19 +8,20 @@ function Contact() {
     return (
         <div className='contactBody'>
             <div className='leftSplit'>
-<div>portrait</div>
+                <div>portrait</div>
             </div>
 
             <div className='rightSplit'>
                 <form>
                     <div>Name</div>
                     <span>
-                    <input type='text' value='First'></input>
-                    <input type='text' value='Last'></input>
+                        <input type='text' value='First'></input>
+                        <input type='text' value='Last'></input>
                     </span>
                     <div>Email</div>
                     <input type='text'></input>
-                    
+                    <div>Comments</div>
+                    <textarea name='comments' rows='10' cols='60'></textarea>
                 </form>
             </div>
         </div>
