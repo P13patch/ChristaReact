a5a2bfbb8cc8035792a1f2446242d44701f8bd32
Patrick Wendel <ptrckwndl13@gmail.com>
2020-01-24 18:18:50 -0700

    building Contact page

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import '../CSS/Contact.css'
+
 
 
 function Contact() {
 
     return (
-        <div>Contact</div>
+        <div className='contactBody'>
+            <div className='leftSplit'>
+<div>portrait</div>
+            </div>
+
+            <div className='rightSplit'>
+                <form>
+                    <div>Name</div>
+                    <span>
+                    <input type='text' value='First'></input>
+                    <input type='text' value='Last'></input>
+                    </span>
+                    <div>Email</div>
+                    <input type='text'></input>
+                    
+                </form>
+            </div>
+        </div>
     )
 }
 
