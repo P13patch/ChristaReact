b4dcae4209be7158b135d61e86ffda93d1765210
Patrick Wendel <ptrckwndl13@gmail.com>
2020-01-31 17:59:42 -0700

    importing photos

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../CSS/Contact.css'
-
+import ChristaPortrait from '../../Images/ChristaPortrait.jpg'
 
 
 function Contact() {
@@ -9,7 +9,9 @@ function Contact() {
         <div className='contactBody'>
 
             <div className='leftSplit'>
-                <div>portrait</div>
+                <div >
+                    <img className="christaPortrait" src={ChristaPortrait}></img>
+                </div>
             </div>
 
 
@@ -18,17 +20,17 @@ function Contact() {
                     <div className="nameBox">
                         <div>Name</div>
                         <span>
-                            <input type='text' name='first' style={{height:"2vw", width:"40%"}}></input>
-                            <input type='text' name='last' style={{height:"2vw", margin:"1vw", width:"40%"}}></input>
+                            <input type='text' name='first' style={{ height: "2vw", width: "40%" }}></input>
+                            <input type='text' name='last' style={{ height: "2vw", margin: "1vw", width: "40%" }}></input>
                         </span>
                     </div>
                     <div className="emailBox">
                         <div>Email</div>
-                        <input type='text' style={{height:"2vw", width:"100%", marginTop:"1vw"}}></input>
+                        <input type='text' style={{ height: "2vw", width: "100%", marginTop: "1vw" }}></input>
                     </div>
                     <div className="commentsBox">
                         <div>Comments</div>
-                        <textarea name='comments' rows='10' cols='60' style={{marginTop:"1vw"}}></textarea>
+                        <textarea name='comments' rows='10' cols='60' style={{ marginTop: "1vw" }}></textarea>
                     </div>
                 </form>
             </div>
