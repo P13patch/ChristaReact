f5278b830a2752e26e1cdc1ee88d30352e124924
Patrick Wendel <ptrckwndl13@gmail.com>
2020-02-05 17:09:46 -0700

    styling contact page

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -8,35 +8,39 @@ function Contact() {
     return (
         <div className='contactBody'>
 
-            <div className="contactHeader">Contact Me</div>
+            <div className='topSplit'>
+                <div >Contact Me</div>
+            </div>
 
-            <div className='leftSplit'>
-                <div >
-                    <img className="christaPortrait" src={ChristaPortrait} alt='Christa Portrait'></img>
+            <div className="contactMiddle">
+                
+                <div className='leftSplit'>
+                    <div >
+                        <img className="christaPortrait" src={ChristaPortrait} alt='Christa Portrait'></img>
+                    </div>
+                </div>
+
+
+                <div className='rightSplit'>
+                    <form>
+                        <div className="nameBox">
+                            <div>Name</div>
+                            <span>
+                                <input type='text' name='first' style={{ height: "2vw", width: "40%" }}></input>
+                                <input type='text' name='last' style={{ height: "2vw", margin: "1vw", width: "40%" }}></input>
+                            </span>
+                        </div>
+                        <div className="emailBox">
+                            <div>Email</div>
+                            <input type='text' style={{ height: "2vw", width: "100%", marginTop: "1vw" }}></input>
+                        </div>
+                        <div className="commentsBox">
+                            <div>Comments</div>
+                            <textarea name='comments' rows='10' cols='60' style={{ marginTop: "1vw" }}></textarea>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-
-            <div className='rightSplit'>
-                <form>
-                    <div className="nameBox">
-                        <div>Name</div>
-                        <span>
-                            <input type='text' name='first' style={{ height: "2vw", width: "40%" }}></input>
-                            <input type='text' name='last' style={{ height: "2vw", margin: "1vw", width: "40%" }}></input>
-                        </span>
-                    </div>
-                    <div className="emailBox">
-                        <div>Email</div>
-                        <input type='text' style={{ height: "2vw", width: "100%", marginTop: "1vw" }}></input>
-                    </div>
-                    <div className="commentsBox">
-                        <div>Comments</div>
-                        <textarea name='comments' rows='10' cols='60' style={{ marginTop: "1vw" }}></textarea>
-                    </div>
-                </form>
-            </div>
-
 
         </div>
     )
