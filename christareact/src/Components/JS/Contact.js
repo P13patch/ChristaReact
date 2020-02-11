e07ea50fb8cb380ec4010496a5a7cc94a73ea1da
Patrick Wendel <ptrckwndl13@gmail.com>
2020-02-10 18:21:00 -0700

    css media styling

--- christareact/src/Components/JS/Contact.js
+++ christareact/src/Components/JS/Contact.js
@@ -8,12 +8,12 @@ function Contact() {
     return (
         <div className='contactBody'>
 
-            <div className='topSplit'>
+            <div className='contactTop'>
                 <div >Contact Me</div>
             </div>
 
             <div className="contactMiddle">
-                
+
                 <div className='leftSplit'>
                     <div >
                         <img className="christaPortrait" src={ChristaPortrait} alt='Christa Portrait'></img>
@@ -23,21 +23,23 @@ function Contact() {
 
                 <div className='rightSplit'>
                     <form>
-                        <div className="nameBox">
-                            <div>Name</div>
-                            <span>
-                                <input type='text' name='first' style={{ height: "2vw", width: "40%" }}></input>
-                                <input type='text' name='last' style={{ height: "2vw", margin: "1vw", width: "40%" }}></input>
-                            </span>
-                        </div>
-                        <div className="emailBox">
-                            <div>Email</div>
-                            <input type='text' style={{ height: "2vw", width: "100%", marginTop: "1vw" }}></input>
-                        </div>
-                        <div className="commentsBox">
-                            <div>Comments</div>
-                            <textarea name='comments' rows='10' cols='60' style={{ marginTop: "1vw" }}></textarea>
-                        </div>
+                        <fieldset style={{borderStyle:'none'}}>
+                            <div className="nameBox">
+                                <div>Name</div>
+                                <span>
+                                    <input type='text' name='first' className='nameFirst'></input>
+                                    <input type='text' name='last' className='nameLast'></input>
+                                </span>
+                            </div>
+                            <div className="emailBox">
+                                <div>Email</div>
+                                <input type='text' className='emailTextField'></input>
+                            </div>
+                            <div className="commentsBox">
+                                <div>Comments</div>
+                                <textarea name='comments' rows='10' className='commentsTextArea'></textarea>
+                            </div>
+                        </fieldset>
                     </form>
                 </div>
             </div>
